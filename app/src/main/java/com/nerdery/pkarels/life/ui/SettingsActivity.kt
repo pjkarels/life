2df0815482113e0cd6a508e0da6734ede7cc4328
Paul Karels <pkarels@nerdery.com>
2018-10-18 22:47:19 -0500

    Convert to Kotlin

---
+++ app/src/main/java/com/nerdery/pkarels/life/ui/SettingsActivity.kt
@@ -0,0 +1,24 @@
+package com.nerdery.pkarels.life.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupActionBar()
+        // Display the fragment as the main content.
+        supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, SettingsFragment())
+                .commit()
+    }
+
+    /**
+     * Set up the [android.app.ActionBar], if the API is available.
+     */
+    private fun setupActionBar() {
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
