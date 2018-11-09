92d02a031562764a136e95c779c058ca34dc6fb2
Paul Karels <pkarels@nerdery.com>
2018-11-09 14:13:01 -0600

    Fill out Transaction ui

--- finance/src/main/java/com/bitbybitlabs/pkarels/finance/TransactionsActivity.kt
+++ finance/src/main/java/com/bitbybitlabs/pkarels/finance/TransactionsActivity.kt
@@ -2,7 +2,6 @@ package com.bitbybitlabs.pkarels.finance
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_transactions.*
 
 class TransactionsActivity : AppCompatActivity() {
@@ -11,11 +10,6 @@ class TransactionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transactions)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
--- finance/src/main/java/com/bitbybitlabs/pkarels/finance/TransactionsFragment.kt
+++ finance/src/main/java/com/bitbybitlabs/pkarels/finance/TransactionsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
 
 
 class TransactionsFragment : Fragment() {
@@ -15,10 +16,18 @@ class TransactionsFragment : Fragment() {
     }
 
     private lateinit var viewModel: TransactionsViewModel
+    private lateinit var contentView: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_transactions, container, false)
+        contentView = inflater.inflate(R.layout.fragment_transactions, container, false)
+
+        val fab = contentView.findViewById<View>(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        return contentView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
